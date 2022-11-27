1b90ffc5f0ea6814c61c3d9ac4dc4d348ef84e7f
prathambande <pratham0802@gmail.com>
2022-11-27 16:00:31 +0530

    removed mongodburi

--- Server/backend/Config/db_config.js
+++ Server/backend/Config/db_config.js
@@ -4,5 +4,5 @@ module.exports = {
   NEXMO_API_KEY: "x",
   NEXMO_API_SECRET: "x",
   NEXMO_FROM_NUMBER: "x",
-  MongoURI: `mongodb+srv://prathambande:<prathambande>@cluster0.d7xm20m.mongodb.net/test?retryWrites=true&w=majority`
+  MongoURI: `x`
 }
